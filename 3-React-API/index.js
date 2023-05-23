690c68cb208cf3333adeffa673df348b39ef01e6
Ivan Sevilla <ivansevillaa@gmail.com>
2023-05-22 21:47:43 -0300

    clase 10

---
+++ 3-React-API/index.js
@@ -0,0 +1,27 @@
+const app = document.getElementById('app')
+
+
+function Artist(props) {
+  return React.createElement('div', { className: 'container' }, [
+    React.createElement('h2', null, props.name),
+    React.createElement('p', { className: 'song' }, props.song),
+  ])
+}
+
+function App() {
+  return React.createElement(
+    'main',
+    null,
+    [
+      React.createElement('h1', { className: 'title' }, 'Music App'),
+      React.createElement(Artist, { name: 'Justin Bieber', song: 'Sorry' }),
+      React.createElement(Artist, { name: 'Dua Lipa', song: 'Levitating' }),
+      React.createElement(Artist, { name: 'Dillom', song: 'Alguna' }),
+    ]
+  )
+}
+
+const root = ReactDOM.createRoot(app)
+
+// app.appendChild(title)
+root.render(React.createElement(App))
