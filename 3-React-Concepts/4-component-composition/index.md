8e02cf5daea7f40cfa92ce5789535ca4fc6bdf81
Ivan Sevilla <ivansevillaa@gmail.com>
2023-06-28 21:37:54 -0300

    clase 19

---
+++ 3-React-Concepts/4-component-composition/index.md
@@ -0,0 +1,33 @@
+```jsx
+// Prop drilling
+// -> Component composition
+// -> Context
+import React, { useState, createContext, useContext } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Counter>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <ComponentB>
+        <ComponentC count={count} />
+      </ComponentB>
+    </Counter>
+  );
+}
+
+function Counter({ children }) {
+  return <div>{children}</div>;
+}
+
+function ComponentB({ children }) {
+  return <div>Layout{children}</div>;
+}
+
+function ComponentC({ count }) {
+  return <div>{count}</div>;
+}
+
+export default App;
+```
--- my-music-app/src/app/screens/Home/index.tsx
+++ my-music-app/src/app/screens/Home/index.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import Artist from "../../components/Artist";
 import Search from "../../components/Search";
 import styles from "./styles.module.css";
+
+const ThemeContext = createContext(undefined);
 
 function Home() {
   const [characters, setCharacters] = useState();
@@ -22,25 +24,27 @@ function Home() {
   }
 
   return (
-    <main>
-      <h1 className={styles.container}>Music Home</h1>
-      <Search />
-      {characters ? (
-        characters.map((character) => {
-          return (
-            <Artist
-              key={character.id}
-              id={character.id}
-              name={character.name}
-              song={character.status}
-              views={character.episode.length}
-            />
-          );
-        })
-      ) : (
-        <h1>Cargando...</h1>
-      )}
-    </main>
+    <ThemeContext.Provider value="dark">
+      <main>
+        <h1 className={styles.container}>Music Home</h1>
+        <Search />
+        {characters ? (
+          characters.map((character) => {
+            return (
+              <Artist
+                key={character.id}
+                id={character.id}
+                name={character.name}
+                song={character.status}
+                views={character.episode.length}
+              />
+            );
+          })
+        ) : (
+          <h1>Cargando...</h1>
+        )}
+      </main>
+    </ThemeContext.Provider>
   );
 }
 
